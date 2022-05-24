7b12bde79bce1e3d38fda66dc736bc3bc0b4beb4
zaraujoz <matheusha@live.com>
2022-05-24 17:45:56 +0000

    Adicionando Arquivos

---
+++ src/main/java/application/models/Genero.java
@@ -0,0 +1,44 @@
+package application.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+import java.util.HashSet;
+
+@Entity
+@Table(name="generos")
+
+public class Genero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    @OneToMany(mappedBy = "genero")
+    private Set<Jogo> jogos = new HashSet<>();
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Set<Jogo> getJogos() {
+        return jogos;
+    }
+    public void setJogos(Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+    
+}
